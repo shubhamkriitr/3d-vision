d356f8d21bf86b3c3b7e3417a48471855118a048
Shubham Kumar <kumarsh@student.ethz.ch>
2022-05-17 15:33:54 +0200

    add timestamp in utils

--- visn/utils/__init__.py
+++ visn/utils/__init__.py
@@ -1,1 +1,7 @@
 from loguru import logger # 
+from datetime import datetime
+
+def get_timestamp_str(granularity=1000):
+    if granularity != 1000:
+        raise NotImplementedError()
+    return datetime.now().strftime("%Y-%m-%d_%H%M%S_")
