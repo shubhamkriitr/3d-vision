8d6bf0a7b4b86b9a635bfb044da322fc9219baa9
Shubham Kumar <kumarsh@student.ethz.ch>
2022-05-17 22:15:30 +0200

    add adhoc tranforms for testing

--- visn/process/pipeline.py
+++ visn/process/pipeline.py
@@ -29,6 +29,7 @@ class BasePipeline:
             
         # ]
         self.steps = [
+            # AdHocTransforms().process,
             self.preprocessor.process,
             self.pose_estimation_processor.process,
             self.benchmarking_processor.process
@@ -61,8 +62,46 @@ class BasePipeline:
 def extract_relevant_info(running_output):
     return running_output  #TODO
 
+
+
+class AdHocTransforms(BasePreprocessor):
+    
+    def __init__(self, config: Dict = ..., **kwargs) -> None:
+        super().__init__(config, **kwargs)
+    
+    def _init_from_config(self, config):
+        pass
+        
+    def process_one_sample(self, sample):
+        x = 1
+        y = x +1
+        import numpy as np
+        from copy import deepcopy
+        common_chosen_axis = np.array([[0], [0], [-1]], dtype=np.float64)
         
         
+        Rt0, Rt1 = sample['input_relative_poses']
+        R0, t0 = Rt0[:, 0:3], Rt0[:, 3:4]
+        R1, t1 = Rt1[:, 0:3], Rt1[:, 3:4]
+        
+        g0 = R0 @ common_chosen_axis
+        g1 = R1 @ common_chosen_axis
+        
+        g0 = g0 / np.linalg.norm(g0)
+        g1 = g1 / np.linalg.norm(g1)
+        
+        g0 = np.ravel(g0)
+        g1 = np.ravel(g1)
+        
+        logger.warning(f"Overriding input_gravity")
+        
+        sample['input_gravity'] = [g0, g1]
+        sample['input_roll_pitch_gt'] = deepcopy(sample['input_gravity'])
+        sample['input_gravity_gt'] = deepcopy(sample['input_gravity'])
+        
+        
+    def compute_dummy_gravity_vector_from_rotation_matrices(self):
+        pass
         
         
 
