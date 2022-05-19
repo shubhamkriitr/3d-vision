e0d019e6adc62970191a723293244fa3feb30a3d
Shubham Kumar <kumarsh@student.ethz.ch>
2022-05-19 16:33:44 +0200

    create placeholder draft for  synthetic generator

--- visn/data/synthetic.py
+++ visn/data/synthetic.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.spatial.transform import Rotation
-
+import os
+import shutil
+import matplotlib
 
 class SyntheticDataGenerator(object):
     def __init__(self) -> None:
@@ -128,6 +130,100 @@ class CameraPairDataGeneratorUpright3Pt(CameraPairDataGenerator):
         rotation = Rotation.from_euler('zyx', [0, 90, 0], degrees=True)
         rotation = rotation.as_matrix().astype(np.float64)
         return rotation
+
+
+DIR_CALIBRATION = "calibration"
+DIR_GRAVITY_GT = "gravity_gt"
+DIR_GRAVITY_PRED = "gravity_pred"
+DIR_IMAGES = "images"
+DIR_REL_POSE = "relative_pose"
+DIR_ROLL_PITCH_GT = "roll_pitch_gt"
+DIR_ROLL_PITCH_PRED = "roll_pitch_pred"
+
+ALL_VISN_DIRS = [
+    DIR_CALIBRATION,
+    DIR_GRAVITY_GT,
+    DIR_GRAVITY_PRED,
+    DIR_IMAGES,
+    DIR_REL_POSE,
+    DIR_ROLL_PITCH_GT,
+    DIR_ROLL_PITCH_PRED
+]
+class SyntheticVisnDataGenerator(object):
+    
+    def __init__(self) -> None:
+        pass
+    
+    def generate(num_pairs, output_dir, *args, **kwargs):
+        pass
+    
+    def save_single_image_data(self, sr_num,
+                               image_id, dtu_scan_dir, target_dir):
+        
+        
+        out_sr_num = str(sr_num).zfill(4)
+        
+        # TODO: @ Implement
+        K = None
+        Rt = None
+        size = None
+        gravity = None
+        image_data = np.ones(shape=tuple(np.ravel(size)))
+        DUMMY_ROLL_PITCH = [0, 0]
+        
+        
+        # paths
+        out_img_path = os.path.join(target_dir, DIR_IMAGES, out_sr_num+".png")
+        out_calib_path, out_pose_path, out_size_path, out_gravity_gt_path,\
+            out_gravity_pred_path, out_rp_gt_path, out_rp_pred_path \
+                = self.create_absolute_output_paths(target_dir, out_sr_num)
+        
+        
+        # save to files
+        matplotlib.image.imsave(out_img_path, image_data)
+        np.savetxt(out_calib_path, K)
+        np.savetxt(out_pose_path, Rt)
+        np.savetxt(out_size_path, size)
+        np.savetxt(out_gravity_gt_path, gravity)
+        np.savetxt(out_gravity_pred_path, gravity)
+        np.savetxt(out_rp_gt_path, DUMMY_ROLL_PITCH)
+        np.savetxt(out_rp_pred_path, DUMMY_ROLL_PITCH)
+    
+    def create_absolute_output_paths(self, target_dir, out_sr_num):
+        out_calib_path = os.path.join(target_dir, DIR_CALIBRATION,
+                                      f"K_{out_sr_num}.txt")
+        out_pose_path = os.path.join(target_dir, DIR_REL_POSE,
+                                     f"{out_sr_num}.txt")
+        out_size_path = os.path.join(target_dir, DIR_CALIBRATION, 
+                                     f"image_size_{out_sr_num}.txt")
+        out_gravity_gt_path = os.path.join(target_dir, DIR_GRAVITY_GT,
+                                           f"{out_sr_num}.txt")
+        out_gravity_pred_path = os.path.join(target_dir, DIR_GRAVITY_PRED,
+                                           f"{out_sr_num}.txt")
+        out_rp_gt_path = os.path.join(target_dir, DIR_ROLL_PITCH_GT,
+                                           f"{out_sr_num}.txt")
+        out_rp_pred_path = os.path.join(target_dir, DIR_ROLL_PITCH_PRED,
+                                           f"{out_sr_num}.txt")
+                                           
+        return out_calib_path,out_pose_path,out_size_path,out_gravity_gt_path,\
+            out_gravity_pred_path,out_rp_gt_path,out_rp_pred_path
+    
+    def init_visn_folder_structure(self, target_dir):
+        os.makedirs(target_dir, exist_ok=True)
+        for dir_name in ALL_VISN_DIRS:
+            os.makedirs(os.path.join(target_dir, dir_name), exist_ok=True)
+    
+    def compute_gravity_from_pose(self, Rt):
+        R = Rt[:, 0:3]
+        
+        g_ref = np.array([[0], [0], [-1]], dtype=np.float64)
+        
+        g_new = R @ g_ref
+        
+        g_new = np.ravel(g_new)
+        
+        return g_new
+    
     
 if __name__ == "__main__":
     datagen = CameraPairDataGenerator()
