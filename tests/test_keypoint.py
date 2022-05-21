cd0abb8b46396e10bddfe2b2e5b2e35a7caa55ef
Nico Burger <nicoburger1998@gmail.com>
2022-05-21 16:54:33 +0200

    keypoint visualizer

--- tests/test_keypoint.py
+++ tests/test_keypoint.py
@@ -10,7 +10,7 @@ data_batch = dataset[0]
 # load and process data
 img_1 = data_batch["input_images"][0]
 img_2 = data_batch["input_images"][1]
-keypoints_1, keypoints_2 = kpm.get_matches(img_1, img_2, ratio=1)
+keypoints_1, keypoints_2 = kpm.get_matches(img_1, img_2, ratio=0.75)
 kp1 = np.array(keypoints_1)
 kp2 = np.array(keypoints_2)
 
@@ -27,8 +27,6 @@ kp2_adj[:, 0] += x_offset
 ax1.imshow(img)
 ax1.scatter(kp1[:, 0], kp1[:, 1], s=point_size, c=point_color)
 ax1.scatter(kp2_adj[:, 0], kp2_adj[:, 1], s=point_size, c=point_color)
-ax1.scatter(kp1[100, 0], kp1[100, 1], s=2, c='r')  # illustrate specific point
-ax1.scatter(kp2_adj[100, 0], kp2_adj[100, 1], s=2, c='r')  # illustrate specific point
 
 # plot axis 2
 ax2.imshow(img)
